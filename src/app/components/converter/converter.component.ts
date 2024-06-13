c551a080678e8a1adf810b999ad30c4444de8212
Janan <fatemeh.javdan@student.intecbrussel.be>
2024-06-13 18:19:17 +0200

    Commit je wijzigingen voordat je Angular update

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CurrencyConversionResponse } from '../../models/currency-conversion-response';
+import { CurrencyConversionResponse } from '../../interfaces/currency-conversion-response';
 import { CurrencyConverterService } from '../../services/CurrencyConvertService';
 
 
